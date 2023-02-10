d88510e3d2fcc7822e81aa0d5ee3e3a68f3945b0
Lucas Amaral Oliveira da Silva <lucas.amaral.oliveira.silva@gmail.com>
2023-02-10 17:58:11 -0300

    feat: included more one convertion type component

--- src/components/pages/PdfxImg/index.jsx
+++ src/components/pages/PdfxImg/index.jsx
@@ -1,58 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import axios from "axios";
 
-
-const PdfToImage = () => {
+const PdfToDocxConverter = () => {
   const [pdf, setPdf] = useState(null);
-  const [image, setImage] = useState(null);
 
   const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-    reader.readAsArrayBuffer(file);
-    reader.onload = () => {
-      setPdf(file);
-      const typedArray = new Uint8Array(reader.result);
-      const pdfDoc = window.pdfjsLib.getDocument(typedArray);
-      pdfDoc.promise.then((pdfDoc) => {
-        pdfDoc.getPage(1).then((page) => {
-          const viewport = page.getViewport({ scale: 1 });
-          const canvas = document.createElement('canvas');
-          const ctx = canvas.getContext('2d');
-          canvas.height = viewport.height;
-          canvas.width = viewport.width;
-          const renderContext = {
-            canvasContext: ctx,
-            viewport: viewport
-          };
-          page.render(renderContext).promise.then(() => {
-            setImage(canvas.toDataURL());
-          });
-        });
-      });
-    };
+    setPdf(event.target.files[0]);
   };
 
   const handleUploadClick = () => {
     document.getElementById("file-input").click();
   };
 
+  const handleConvertToDocx = () => {
+    if (!pdf) return;
+
+    const formData = new FormData();
+    formData.append("file", pdf);
+    formData.append("inputformat", "pdf");
+    formData.append("outputformat", "docx");
+
+    axios
+      .post("https://api.cloudconvert.com/convert", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          "Api-Key": "SUA_CHAVE_DE_API_AQUI"
+        }
+      })
+      .then(response => {
+        const url = response.data.output.url;
+        window.location.href = url;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
   return (
     <div>
-      <h1>(PDF x Image)</h1>
+      <h1>(PDF x DOCX)</h1>
       <label htmlFor="file-input">
-        <button type="button" onClick={handleUploadClick}>Escolha um arquivo</button>
+        <button type="button" className="custom-file-upload" onClick={handleUploadClick}>
+          Escolha o PDF
+        </button>
       </label>
-      <input type="file" id="file-input" accept="application/pdf" onChange={handleFileChange} />
-      {image && (
-        <div className="result-section">
-          <h3 className="result-title">PDF original</h3>
-          <embed src={URL.createObjectURL(pdf)} type="application/pdf" />
-          <h3 className="result-title">Resultado em Imagem</h3>
-          <img src={image} alt="Resultado" />
-        </div>
+      <input type="file" id="file-input" onChange={handleFileChange} />
+      {pdf && (
+        <button className="btn-download" onClick={handleConvertToDocx}>
+          Baixar
+        </button>
       )}
     </div>
   );
 };
 
-export default PdfToImage;
+export default PdfToDocxConverter;
